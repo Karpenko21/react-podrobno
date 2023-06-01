abce6566fa76e037c5fefd55be51b1df8be6adfd
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-06-01 08:59:05 +0300

    Video #13

--- taskbox/src/stories/Input.stories.tsx
+++ taskbox/src/stories/Input.stories.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
 
-import {} from "../../../src/component/UncontrolledRating/UncontrolledRating";
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -27,19 +27,49 @@ export const TrackValueControlledInputWith = () => {
     )
 }
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    return (
+        <input value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)}
+        />
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(false)
+    return (
+        <input type="checkbox" checked={parentValue} onChange={(e) => setParentValue(e.currentTarget.checked)}
+        />
+    )
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Moscow</option>
+        <option value={"1"}>Kiev</option>
+    </select>
+}
+
 
 export const GetValueControlledInputByButtonPress = () => {
+
     const [value, setValue] = useState("")
     const inputRef = useRef<HTMLInputElement>(null)
     const save = () => {
         const el = inputRef.current as HTMLInputElement
         setValue(el.value)
     }
-    return (
-        <>
-            <input ref={inputRef}/>
-            <button onClick={save}>save</button>
-            - actual value: {value}
-        </>
-    )
+
+    return <>
+        <input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - actual value: {value}
+    </>
 }
