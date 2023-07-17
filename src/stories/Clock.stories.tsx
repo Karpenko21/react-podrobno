0f5e2dbe7725ad82b9095ef3732b72180ac96285
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-07-17 19:51:59 +0600

    Video #23

---
+++ src/stories/Clock.stories.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {Clock} from "../component/Clock/Clock";
+
+
+export default {
+    title: 'components/Clock',
+    component: Clock,
+    tags: ['autodocs'],
+};
+
+
+export const BaseExample = () => {
+
+    return <Clock/>
+}
+
